6334d4ba7b5277b56f8685c663902d73451ee588
Benjamin Zhang <59369257+BLAZEGOD420@users.noreply.github.com>
2022-05-05 19:10:52 -0400

    Update FourierTransform.java

--- FourierTransform.java
+++ FourierTransform.java
@@ -8,6 +8,12 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import javafx.scene.media.Track;
+
+import java.net.URL;
+
 
 public class FourierTransform {
     final static float SAMPLE_RATE = 44100.0F;
@@ -16,7 +22,7 @@ public class FourierTransform {
 
     final static int SAMPLE_SIZE = 8;
 
-    final static int TRANSFORM_SIZE = 16;
+    final static int TRANSFORM_SIZE = 128;
 
     final static double TRANSFOM_TIME = 0.5 * SAMPLE_TIME * TRANSFORM_SIZE;  
 
@@ -24,18 +30,48 @@ public class FourierTransform {
 
     private AudioInputStream stream;
 
-    Buffer b = new Buffer(TRANSFORM_SIZE);
+    int[] buffer = new int[TRANSFORM_SIZE];
     public FourierTransform(AudioInputStream stream){
         this.stream = stream;
-        b.readBegining(stream);
     }
 
-    
+    private long lastReadSample = 0;
+
+    public void reset(){
+        lastReadSample = 0;
+    }
+
+    final double NANO = Math.pow(10,-9);
+    public long sampleAtTime(long time){
+        double t = NANO * time * SAMPLE_RATE;
+        long sample = (long) t;
+        return sample;
+    }
+
+    public void read(long time){
+        try{
+            long sample = sampleAtTime(time);
+            if(sample < lastReadSample){
+                return;
+            }
+            stream.skip(sample - lastReadSample);
+
+            for(int i = 0; i < TRANSFORM_SIZE; ++i){
+                buffer[i] = stream.read();
+            }
+
+            lastReadSample += TRANSFORM_SIZE;
+        }catch(Exception e){
+            //make beter code
+            e.printStackTrace();
+        }
+    }
+
     public Complex fft(double f){
         Complex sum = Complex.ZERO;
         for(int i = 0; i < TRANSFORM_SIZE; ++i){
             double n = i * SAMPLE_TIME - TRANSFOM_TIME;
-            Complex sample = new Complex(b.get(i), 0);
+            Complex sample = new Complex(buffer[i], 0);
             Complex change = sample.times(
                 new Complex(Math.cos(-2*Math.PI*f*n),Math.sin(-2*Math.PI*f*n))
             ).times(SAMPLE_TIME);
@@ -44,37 +80,60 @@ public class FourierTransform {
         return sum;    
     }
 
-    public void next(){
-        try {
-            b.shiftIn( (byte) stream.read());
-        } catch (IOException e) {
+    public static AudioInputStream getAisFromFile(String url){
+        
+        try{
+            URL soundURL = ClassLoader.getSystemResource(url);
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL);
+            return AudioSystem.getAudioInputStream(FORMAT, ais);
+        }catch(Exception e){
             // blah blah blah make better code
             e.printStackTrace();
+            return null;
         }
+        
     }
 
 
-    static TargetDataLine targetDataLine;
-    static Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
-    public static void main(String args[]) throws LineUnavailableException, InterruptedException, IOException{
-        Mixer mixer_ = AudioSystem.getMixer(mixerInfo[0]);
 
-        DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, FORMAT);
 
-        Mixer mixer = AudioSystem.getMixer(mixerInfo[2]);
-        targetDataLine = (TargetDataLine) mixer.getLine(dataLineInfo);
-        targetDataLine.open(FORMAT);
-        targetDataLine.start();
+
+    // static TargetDataLine targetDataLine;
+    // static Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
+
+
+    public static void main(String args[]) throws LineUnavailableException, InterruptedException, IOException, UnsupportedAudioFileException{
+        // Mixer mixer_ = AudioSystem.getMixer(mixerInfo[0]);
+
+        // DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, FORMAT);
+
+        // Mixer mixer = AudioSystem.getMixer(mixerInfo[2]);
+        // targetDataLine = AudioSystem.getTargetDataLine(FORMAT);//(TargetDataLine) mixer.getLine(dataLineInfo);
+        // targetDataLine.open(FORMAT);
+        // targetDataLine.start();
         
-        AudioInputStream ai = new AudioInputStream(targetDataLine);
-        FourierTransform f = new FourierTransform(ai);
+        // AudioInputStream ai = new AudioInputStream(targetDataLine);
+        // FourierTransform f = new FourierTransform(ai);
 
-        double test = 440;
+        // double test = 440;
 
-        while(true){
-            f.next();
-            System.out.println(f.fft(test).re());
-            Thread.sleep(10);
+        // while(true){
+        //     f.next();
+        //     System.out.println(f.fft(test).phase());
+        //     Thread.sleep(100);
+        // }
+        AudioInputStream ais = getAisFromFile("./SoundFile.wav");
+        boolean go = true;
+        int count = 0;
+        System.out.println(ais.getFormat());
+        while(go){
+            if(ais.read() == -1){
+                go = false;
+            }
+            //System.out.println(cringe[0]);
+            count++;
+            //Thread.sleep(10);
         }
+        System.out.println(count);
     }
 }
