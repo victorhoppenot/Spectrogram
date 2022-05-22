cc68e117f74bc71aca773d6e7994875a6523f134
Benjamin Zhang <59369257+BLAZEGOD420@users.noreply.github.com>
2022-05-22 00:10:45 -0400

    Update Instance.java

--- Instance.java
+++ Instance.java
@@ -1,7 +1,10 @@
 import java.io.File;
+import javafx.util.Duration;
+import java.util.function.BiFunction;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.media.*;
+import javafx.scene.media.MediaPlayer.Status;
 
 public class Instance {
     File file;
@@ -9,6 +12,7 @@ public class Instance {
     private FourierTransform ft;
     private MediaPlayer mp;
     private Animator timer;
+    private Duration pauseTime = new Duration(0);
 
     
     public Instance(File file, GraphicsContext pen){
@@ -16,8 +20,14 @@ public class Instance {
         Media sound = new Media(file.toURI().toString());
         mp = new MediaPlayer(sound);
         ft = new FourierTransform(FourierTransform.getAisFromFile(file));
-        timer = Animators.BLACK_LINE(pen, ft);
+        timer = new Animator(pen, ft);
     }
+
+    public void constructStyle(BiFunction<GraphicsContext, FourierTransform, Style> construct){
+        timer.constructStyle(construct);
+
+    }
+
 
     public void start(){
         mp.play();
@@ -29,8 +39,16 @@ public class Instance {
         timer.stop();
     }
 
-    public void pause(){
-        mp.pause();
-        timer.stop();
+    public void unpause(){
+
+        mp.setStartTime(pauseTime);
+        mp.play();
+        timer.start();
     }
+    public void pause(){
+        pauseTime = mp.getCurrentTime();
+        mp.stop();
+        timer.pause();
+    }
+    
 }
